9c0b25eff1223136a304b00a584604de0d402d1d
Sadra <31988724+xsadra@users.noreply.github.com>
2021-05-13 22:10:27 +0200

    Implement ProfileRepository

--- lib/clash/data/datasources/datasources.dart
+++ lib/clash/data/datasources/datasources.dart
@@ -1,2 +1,4 @@
 export 'upcoming_chest_local_data_source.dart';
 export 'upcoming_chest_remote_data_source.dart';
+export 'profile_local_data_source.dart';
+export 'profile_remote_data_source.dart';
---
+++ lib/clash/data/repositories/profile_repository_impl.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../../core/error/exceptions.dart';
+import '../../../core/error/failure.dart';
+import '../../../core/platform/network_info.dart';
+import '../../domain/entities/profile.dart';
+import '../../domain/repository/profile_repository.dart';
+import '../datasources/datasources.dart';
+
+class ProfileRepositoryImpl implements ProfileRepository {
+  final NetworkInfo networkInfo;
+  final ProfileLocalDataSource localDataSource;
+
+  final ProfileRemoteDataSource remoteDataSource;
+
+  const ProfileRepositoryImpl({
+    @required this.networkInfo,
+    @required this.localDataSource,
+    @required this.remoteDataSource,
+  });
+
+  @override
+  Future<Either<Failure, Profile>> getProfile(String playerTag) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteProfile =
+        await remoteDataSource.getProfile(playerTag);
+        localDataSource.cacheProfile(remoteProfile);
+        return right(remoteProfile);
+      } on ServerException {
+        return left(ServerFailure());
+      }
+    }
+    try {
+      final localProfile = await localDataSource.getLastData();
+      return Right(localProfile);
+    } on CacheException {
+      return Left(CacheFailure());
+    }
+  }
+}
