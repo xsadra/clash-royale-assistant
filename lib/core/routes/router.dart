9e3a1527a847f88a4b688abefd3b36d1f5705b87
Sadra <31988724+xsadra@users.noreply.github.com>
2021-05-28 22:56:12 +0200

    Add Routing to the App using [auto_route]

---
+++ lib/core/routes/router.dart
@@ -0,0 +1,30 @@
+import 'package:auto_route/auto_route_annotations.dart';
+
+import '../../clash/presentation/pages/home_page.dart';
+import '../../clash/presentation/pages/input_tag_page.dart';
+import '../../clash/presentation/pages/input_tag_page_full.dart';
+import '../../clash/presentation/pages/splash_page.dart';
+
+@MaterialAutoRouter(
+    generateNavigationHelperExtension: true,
+    routes: <AutoRoute>[
+      MaterialRoute(
+        page: SplashPage,
+        name: 'SplashPageRoute',
+        initial: true,
+      ),
+      MaterialRoute(
+        page: InputTagPage,
+        name: 'InputTagPageRoute',
+      ),
+      MaterialRoute(
+        page: InputTagPageFull,
+        name: 'InputTagPageFullScreenRoute',
+        fullscreenDialog: true,
+      ),
+      MaterialRoute(
+        page: HomePage,
+        name: 'HomePageRoute',
+      ),
+    ])
+class $Router {}
