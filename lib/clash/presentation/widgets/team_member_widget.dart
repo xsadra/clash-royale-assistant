dc2c43e5f5e87269f27f5dd18f91d768a638394b
Sadra <31988724+xsadra@users.noreply.github.com>
2021-06-17 08:27:51 +0200

    Create TeamMemberWidget to showing team deck

---
+++ lib/clash/presentation/widgets/team_member_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart' hide Card;
+
+import '../../../core/platform/assets_controller.dart';
+import '../../domain/entities/battle.dart';
+import '../../domain/entities/card.dart';
+import 'widgets.dart';
+
+class TeamMemberWidget extends StatelessWidget {
+  const TeamMemberWidget({
+    Key key,
+    @required this.team,
+    @required this.teamColor,
+  }) : super(key: key);
+
+  final TeamMember team;
+  final Color teamColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Card> teamCards = team.cards;
+    return Container(
+      color: teamColor,
+      child: Column(
+        children: [
+          Text(team.name),
+          Row(
+            children: [
+              Assets.clanBadgeIdToImage(team.clan.badgeId, size: 24),
+              Text(team.clan.name),
+            ],
+          ),
+          Row(
+            children: [
+              CardAssetImageBattle(card: teamCards[0]),
+              CardAssetImageBattle(card: teamCards[1]),
+              CardAssetImageBattle(card: teamCards[2]),
+              CardAssetImageBattle(card: teamCards[3]),
+            ],
+          ),
+          Row(
+            children: [
+              CardAssetImageBattle(card: teamCards[4]),
+              CardAssetImageBattle(card: teamCards[5]),
+              CardAssetImageBattle(card: teamCards[6]),
+              CardAssetImageBattle(card: teamCards[7]),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
