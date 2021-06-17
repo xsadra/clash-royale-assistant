88d1a0e3ed3fc7b4b3724bcefd404a5ffb6d652a
Sadra <31988724+xsadra@users.noreply.github.com>
2021-06-18 00:13:04 +0200

    Update Assets Controller [clanBadgeIdToImage, cardToImage, crownRed, crownBlue]

--- lib/core/platform/assets_controller.dart
+++ lib/core/platform/assets_controller.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart' hide Card;
+
+import '../../clash/domain/entities/card.dart';
+
 class Assets {
   Assets._();
+
   static String get experience => 'assets/images/ui/ui-experience.png';
+
   static String get starLevel => 'assets/images/ui/ui-star-level.png';
+
   static String get notFound => 'assets/images/ui/not-found.png';
+
+  static String get crownRed => 'assets/images/crown/crown-red.png';
+
+  static String get crownBlue => 'assets/images/crown/crown-blue.png';
 
   static String chestNameToPath(String name) {
     final images = {
@@ -24,6 +35,8 @@ class Assets {
     return 'assets/images/arena/${id.toString()}.png';
   }
 
+  @Deprecated(
+      'Use `cardToImage` instead. This method cause an ERROR, on parsing missing Card')
   static String cardToPath(String name, {bool isGold}) {
     String path = isGold ? 'assets/images/cards-gold/' : 'assets/images/cards/';
     String fileName =
@@ -32,7 +45,36 @@ class Assets {
     return path + fileName;
   }
 
+  static Image cardToImage(Card card) {
+    String path = card.starLevel != null
+        ? 'assets/images/cards-gold/'
+        : 'assets/images/cards/';
+    String fileName =
+        card.name.replaceAll(' ', '-').replaceAll('.', '').toLowerCase() +
+            '.png';
+
+    return Image.asset(
+      path + fileName,
+      errorBuilder: (context, error, stackTrace) {
+        return Image.network(card.iconUrls.medium);
+      },
+    );
+  }
+
   static String clanBadgeIdToPath(int id) {
     return 'assets/images/clan-badges/${id.toString()}.png';
+  }
+
+  static Widget clanBadgeIdToImage(int id, {double size}) {
+    String path = 'assets/images/clan-badges/${id.toString()}.png';
+    if (size == null) {
+      return Image.asset(path);
+    }
+    assert(size > 0);
+    return SizedBox(
+      width: size,
+      height: size,
+      child: Image.asset(path),
+    );
   }
 }
