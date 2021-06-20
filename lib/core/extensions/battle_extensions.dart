f9e9666bbfdb05a3b609c44b3a3228774dec434f
Sadra <31988724+xsadra@users.noreply.github.com>
2021-06-20 12:07:26 +0200

    Create BattleEntityExtensions on Battle

---
+++ lib/core/extensions/battle_extensions.dart
@@ -0,0 +1,53 @@
+import '../../clash/domain/entities/battle.dart';
+import '../enums/enums_with_extensions.dart';
+import 'string_extensions.dart';
+
+extension BattleEntityExtensions on Battle {
+  bool get hasCrowns =>
+      this.team[0].crowns != null && this.opponent[0].crowns != null;
+
+  bool get hasCrowns2v2 =>
+      this.team[0].crowns != null &&
+      this.opponent[0].crowns != null &&
+      this.opponent[1].crowns != null &&
+      this.opponent[1].crowns != null;
+
+  @Deprecated('use [battleResult] instead')
+  bool get didTeamWin =>
+      (this.team[0].crowns ?? 0) > (this.opponent[0].crowns ?? 0);
+
+  String get winCount => (this.challengeWinCountBefore ?? 'no').toString();
+
+  String get timeAgo => this.battleTime.timeAgo();
+
+  @Deprecated('use [battleResult] instead')
+  String get resultTitle => this.didTeamWin ? 'Victory' : 'Defeat';
+
+  String get battleResultTitle => battleResult.title;
+
+  BattleResult get battleResult {
+    if (this.teamCrowns == this.opponentCrowns) {
+      return BattleResult.Draw;
+    } else if (this.teamCrowns > this.opponentCrowns) {
+      return BattleResult.Victory;
+    }
+    return BattleResult.Defeat;
+  }
+
+  String get winCountText =>
+      (this.challengeWinCountBefore ?? 0) > 0 ? ' Wins' : ' Win';
+
+  String get gameModeName => this.gameMode.name;
+
+  int get teamCrowns => this.team1.crowns ?? 0;
+
+  int get opponentCrowns => this.opponent1.crowns ?? 0;
+
+  TeamMember get team1 => this.team[0];
+
+  TeamMember get opponent1 => this.opponent[0];
+
+  TeamMember get team2 => this.team[1] ?? null;
+
+  TeamMember get opponent2 => this.opponent[1] ?? null;
+}
