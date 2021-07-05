f8780ea1257a84dd9f53fde2dce364e955333138
Sadra <31988724+xsadra@users.noreply.github.com>
2021-07-06 00:50:48 +0200

    Add 'TwoToTwoBattleTeam' to display battles result

---
+++ lib/clash/presentation/widgets/two_to_two_battle_team_widget.dart
@@ -0,0 +1,77 @@
+import 'package:clash_royale_assistant/core/constants/consts.dart';
+import 'package:flutter/material.dart' hide Card;
+
+import '../../../core/enums/enums_with_extensions.dart';
+import '../../../core/extensions/extensions.dart';
+import '../../domain/entities/battle.dart';
+import 'widgets.dart';
+
+class TwoToTwoBattleTeam extends StatelessWidget {
+  const TwoToTwoBattleTeam({
+    Key key,
+    @required this.battle,
+  }) : super(key: key);
+
+  final Battle battle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TeamMemberWidget(
+              team: battle.team1,
+              teamType: TeamMemberType.Team,
+              showTrophies: battle.hasTrophies,
+            ),
+            VerticalDividerWidget(
+                height: 170.0, thickness: 2.0, color: Colors.black38),
+            TeamMemberWidget(
+              team: battle.opponent1,
+              teamType: TeamMemberType.opponent,
+              showTrophies: battle.hasTrophies,
+            ),
+          ],
+        ),
+        Container(
+          padding: EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            color: Colors.white38,
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(
+              color: Colors.white38, // red as border color
+            ),
+          ),
+          child: Text(
+            AppTexts.ui.vsCap,
+            style: TextStyle(
+              fontSize: 10,
+              color: Colors.black54,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        AppStyles.sizedBox.height4,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TeamMemberWidget(
+              team: battle.team2,
+              teamType: TeamMemberType.Team,
+              showTrophies: battle.hasTrophies2v2,
+            ),
+            VerticalDividerWidget(
+                height: 170.0, thickness: 2.0, color: Colors.black38),
+            TeamMemberWidget(
+              team: battle.opponent2,
+              teamType: TeamMemberType.opponent,
+              showTrophies: battle.hasTrophies2v2,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
